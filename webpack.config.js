feadd4b5d52551fc451fea7a6a4e821eb8ce0fd8
varsha.rani <varsha2008sigh@gmail.com>
2020-11-06 11:34:20 +0530

    updated webpack file

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
+const path = require('path');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: "./src/app.js",
+  node: {
+      fs: 'empty',
+      net: 'empty',
+      tls: 'empty',
+      global: true,
+      crypto: "empty",
+      process: true,
+      module: false,
+      clearImmediate: false,
+      setImmediate: false
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+        use: [
+            {
+                loader: 'file-loader'
+            },
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
