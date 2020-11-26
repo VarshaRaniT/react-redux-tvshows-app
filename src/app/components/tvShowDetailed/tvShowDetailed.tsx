573fc3e316f15751aca2b23eb66fd6e458a65cef
varsha.rani <varsha2008sigh@gmail.com>
2020-11-26 09:03:31 +0530

    updated back link on detailed page

--- src/app/components/tvShowDetailed/tvShowDetailed.tsx
+++ src/app/components/tvShowDetailed/tvShowDetailed.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import { connect } from "react-redux";
+import {Link} from "react-router-dom"
 import { RootState } from '../../reducers/state';
 import { TvShowProps, TvShowState } from '../../models';
 import { getTvShowDetailed } from '../../actions/tvShowAction';
 import { fetchDetailedTvShow } from "../../data/api/detailedTvShow";
 import { makeStyles, Grid, CardContent, Typography, Card, Container } from '@material-ui/core';
 import StarIcon from '@material-ui/icons/Star';
+import BackIcon from '@material-ui/icons/ArrowBack'
 import DetailedCard from "../searchCard/searchCard";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
@@ -39,7 +41,7 @@ const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
         marginTop: "90px",
-        marginBottom:"40px",
+        marginBottom: "40px",
     },
     media: {
         height: 360,
@@ -61,57 +63,59 @@ const TvShowDetailedComponent: React.FC<TvShowDetailedComponentProps> = ({ searc
     return (
         // render component
         <div className="detailed__component__wrap">
-             <div className={classes.root}> 
-            {searchlist.length === 0 ?<Container><Grid container spacing={3} key={tvshowdetailedlist.id}>
-                <Card className="custom__card__wrap">
-                    <Grid container spacing={3} item xs={12}>
+            <div className={classes.root}>
+                {searchlist.length === 0 ? <Container>
+                    <Grid container spacing={3} key={tvshowdetailedlist.id}>
+                        <Card className="custom__card__wrap">
+                            <Link to="/"><Grid><BackIcon color="secondary"/></Grid></Link>
+                            <Grid container spacing={3} item xs={12}>
 
-                        <Grid item xl={3} lg={5} md={6} sm={12} xs={12}>
-                            <div className="img__wrapper">{tvshowdetailedlist.image ? <img className="border__frame" src={tvshowdetailedlist.image && tvshowdetailedlist.image.original} alt="tv show" /> : "Image not found"}</div>
-                        </Grid>
+                                <Grid item xl={3} lg={5} md={6} sm={12} xs={12}>
+                                    <div className="img__wrapper">{tvshowdetailedlist.image ? <img className="border__frame" src={tvshowdetailedlist.image && tvshowdetailedlist.image.original} alt="tv show" /> : "Image not found"}</div>
+                                </Grid>
 
-                        <Grid item xl={9} lg={7} md={6} sm={12} xs={12} className="content__wrapper">
-                            <CardContent className={`${classes.pb0}`}>
-                                <Typography gutterBottom variant="h4" component="h2" className="main__heading">
-                                    {tvshowdetailedlist.name}
-                                </Typography>
-                                <Typography gutterBottom variant="h6" component="h2">
-                                    Rating
+                                <Grid item xl={9} lg={7} md={6} sm={12} xs={12} className="content__wrapper">
+                                    <CardContent className={`${classes.pb0}`}>
+                                        <Typography gutterBottom variant="h4" component="h2" className="main__heading">
+                                            {tvshowdetailedlist.name}
                                         </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p" className="desc__text__detailed rating">
-                                    {tvshowdetailedlist.rating && tvshowdetailedlist.rating.average ? <StarIcon className="custom__icon" /> : "No Rating"}{tvshowdetailedlist.rating && tvshowdetailedlist.rating.average}
-                                </Typography>
-                                <Typography gutterBottom variant="h6" component="h2">
-                                    Language
+                                        <Typography gutterBottom variant="h6" component="h2">
+                                            Rating
                                         </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p" className="desc__text__detailed">
-                                    {tvshowdetailedlist.language}
-                                </Typography>
-                                <Typography gutterBottom variant="h6" component="h2">
-                                    Summary
+                                        <Typography variant="body2" color="textSecondary" component="p" className="desc__text__detailed rating">
+                                            {tvshowdetailedlist.rating && tvshowdetailedlist.rating.average ? <StarIcon className="custom__icon" /> : "No Rating"}{tvshowdetailedlist.rating && tvshowdetailedlist.rating.average}
                                         </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p" className="desc__text__detailed">
-                                    {tvshowdetailedlist.summary && tvshowdetailedlist.summary.replace(/(<([^>]+)>)/ig, "")}
-                                </Typography>
-                                <Typography gutterBottom variant="h6" component="h2">
-                                    Official Site
+                                        <Typography gutterBottom variant="h6" component="h2">
+                                            Language
                                         </Typography>
-                                <Typography variant="body2" color="textSecondary" component="p" className="desc__text__detailed">
-                                    <a className="custom_link" href={tvshowdetailedlist.officialSite} target="_blank">Official Link</a>
-                                </Typography>
-                            </CardContent>
-                        </Grid>
-                    </Grid>
-                </Card>
-            </Grid></Container>: <Carousel swipeable={false} centerMode={true}
-                draggable={false} responsive={responsive} containerClass="show__container__wrapper custom__container__spacing">
-                {searchlist.map((value: any) => (
-                    <Grid key={value.show.id} className="card__wrap">
-                        <DetailedCard carddata={value} />
-                    </Grid>
-                ))}
-            </Carousel>}
-            </div> 
+                                        <Typography variant="body2" color="textSecondary" component="p" className="desc__text__detailed">
+                                            {tvshowdetailedlist.language}
+                                        </Typography>
+                                        <Typography gutterBottom variant="h6" component="h2">
+                                            Summary
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p" className="desc__text__detailed">
+                                            {tvshowdetailedlist.summary && tvshowdetailedlist.summary.replace(/(<([^>]+)>)/ig, "")}
+                                        </Typography>
+                                        <Typography gutterBottom variant="h6" component="h2">
+                                            Official Site
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p" className="desc__text__detailed">
+                                            <a className="custom_link" href={tvshowdetailedlist.officialSite} target="_blank">Official Link</a>
+                                        </Typography>
+                                    </CardContent>
+                                </Grid>
+                            </Grid>
+                        </Card>
+                    </Grid></Container> : <Carousel swipeable={false} centerMode={true}
+                        draggable={false} responsive={responsive} containerClass="show__container__wrapper custom__container__spacing">
+                        {searchlist.map((value: any) => (
+                            <Grid key={value.show.id} className="card__wrap">
+                                <DetailedCard carddata={value} />
+                            </Grid>
+                        ))}
+                    </Carousel>}
+            </div>
         </div>
     )
 }
